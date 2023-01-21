40178d9c38540ad533ee520fdc40ab861f01ce86
bingxueshuang <manojna.gopher@gmail.com>
2023-01-21 13:02:57 +0530

    feat: describe deque interface

---
+++ interface.go
@@ -0,0 +1,19 @@
+package deque
+
+import "errors"
+
+var (
+	ErrUnderflow = errors.New("deque underflow")
+	ErrIndexBounds = errors.New("deque index out of bounds")
+)
+
+type Interface[T any] interface {
+	func Back() (T, error)
+	func Clear()
+	func Front() (T, error)
+	func Len() int
+	func PopBack() (T, error)
+	func PopFront() (T, error)
+	func PushBack(x T)
+	func PushFront(x T)
+}
