eb00522fb5dcd8035ecb19556e7748a302a31206
bingxueshuang <manojna.gopher@gmail.com>
2023-01-26 11:43:25 +0530

    fix: use circular doubly linked list

--- list.go
+++ list.go
@@ -1,1 +1,25 @@
 package deque
+
+type listNode[T any] struct {
+	value T
+	prev *listNode
+	next *listNode
+}
+
+type Deque[T any] struct {
+	nil *listNode
+	size int
+}
+
+func (d *Deque[T]) init() {
+	node = &listNode[T]{}
+	node.prev = node
+	node.next = node
+	d.nil = node
+}
+
+func New[T any]() *Deque[T] {
+	deque := &Deque[T]{}
+	deque.init()
+	return deque
+}
