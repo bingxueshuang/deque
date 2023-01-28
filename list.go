2cef299b5115a8821d72e5ab50d99b83ae63dbcf
manojnakp <89296644+manojnakp@users.noreply.github.com>
2023-01-28 12:12:32 +0530

    fix: comments

--- list.go
+++ list.go
@@ -1,16 +1,22 @@
 package deque
 
+// listNode is node of circular doubly linked list
 type listNode[T any] struct {
 	value T
 	prev *listNode
 	next *listNode
 }
 
+// Deque is double ended queue data structure implemented
+// using circular doubly linked list. Deque satisfies Interface.
 type Deque[T any] struct {
 	nil *listNode
 	size int
 }
 
+// init fixes the deque by (re)initialising the deque sentinel
+// with proper list node. This should be called when sentinel
+// is nil.
 func (d *Deque[T]) init() {
 	node = &listNode[T]{}
 	node.prev = node
@@ -18,6 +24,7 @@ func (d *Deque[T]) init() {
 	d.nil = node
 }
 
+// New creates and initialises an empty deque ready to use.
 func New[T any]() *Deque[T] {
 	deque := &Deque[T]{}
 	deque.init()
